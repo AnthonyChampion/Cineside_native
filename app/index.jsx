8288070fe340eba43b7dc9c7650bbe99fdbfccec
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-05-22 12:40:27 +0200

    Add fonts working

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
     return (
         <View className="flex-1 justify-center items-center bg-zinc-900">
-            <Text className="text-3xl text-white">Cineside</Text>
+            <Text className="text-3xl text-yellow-400 font-pblack">Cineside</Text>
             <StatusBar style="auto" />
             <Link className="text-white" href="/profile">Go to Profile</Link>
         </View>
