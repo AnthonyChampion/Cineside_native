33793c9847b3bcd35239f1dee4bb465899656498
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-05-22 17:12:59 +0200

    Add Sign In & Sign up structure & style

--- app/index.jsx
+++ app/index.jsx
@@ -25,9 +25,3 @@ export default function App() {
     );
 }
 
-
-{/* <View className="flex-1 justify-center items-center bg-zinc-900">
-<Text className="text-3xl text-yellow-400 font-pblack">Cineside</Text>
-<StatusBar style="auto" />
-<Link className="text-white" href="/home">Go to Home</Link>
-</View> */}
